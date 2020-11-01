efd3b563917997d563628dd532153c87ca1423f1
jtc23 <936865249@qq.com>
2020-11-01 16:29:40 +0800

    initialize the front-end vue project definite the vue-router and axios utils

---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div>homeHeader</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+           
+        },
+      
+    }
+</script>
+
+<style>
+    .homeRouterView {
+        margin-top: 10px;
+    }
+
+    .homeWelcome {
+        text-align: center;
+        font-size: 30px;
+        font-family: 华文行楷;
+        color: #409eff;
+        padding-top: 50px;
+    }
+
+    .homeHeader {
+        background-color: #409eff;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0px 15px;
+        box-sizing: border-box;
+    }
+
+    .homeHeader .title {
+        font-size: 30px;
+        font-family: 华文行楷;
+        color: #ffffff
+    }
+
+    .homeHeader .userInfo {
+        cursor: pointer;
+    }
+
+    .el-dropdown-link img {
+        width: 48px;
+        height: 48px;
+        border-radius: 24px;
+        margin-left: 8px;
+    }
+
+    .el-dropdown-link {
+        display: flex;
+        align-items: center;
+    }
+</style>
---
+++ src/views/HrInfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div>hr</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Hr",
+        data() {
+           
+        },
+      
+    }
+</script>
+
+<style>
+  
+</style>
