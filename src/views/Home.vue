45afc8f50500c058935f85ec494bea352e86074b
jtc23 <936865249@qq.com>
2020-11-02 15:54:15 +0800

    write model of home page and home router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,37 @@
 <template>
     <div>
-        <div>homeHeader</div>
+        <el-container>
+            <el-header class="homeHeader">
+                <div class="title">Back-end Admin</div>
+                <div>
+                    <el-button icon="el-icon-bell" type="text" style="margin-right: 8px;color: #000000;" size="normal" @click="goChat"></el-button>
+                    <el-dropdown class="userInfo" @command="commandHandler">
+  <span class="el-dropdown-link">
+    {{user.name}}<i><img :src="user.userface" alt=""></i>
+  </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item command="userinfo">Personal center</el-dropdown-item>
+                            <el-dropdown-item command="setting">Settings</el-dropdown-item>
+                            <el-dropdown-item command="logout" divided>Login/Login out</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </div>
+            </el-header>
+             </el-container>
+             <el-container>
+                 <div class="container">
+                     <el-main>
+                    <el-breadcrumb separator-class="el-icon-arrow-right" v-if="this.$router.currentRoute.path!='/home'">
+                        <el-breadcrumb-item :to="{ path: '/home' }">Home</el-breadcrumb-item>
+                        <el-breadcrumb-item>{{this.$router.currentRoute.name}}</el-breadcrumb-item>
+                    </el-breadcrumb>
+                    <div class="homeWelcome" v-if="this.$router.currentRoute.path=='/home'">
+                        欢迎来到微人事！
+                    </div>
+                    <router-view class="homeRouterView"/>
+                </el-main>
+                 </div>
+            </el-container>
     </div>
 </template>
 
@@ -8,13 +39,52 @@
     export default {
         name: "Home",
         data() {
-           
+            return {
+                // user: JSON.parse(window.sessionStorage.getItem("user"))
+            }
         },
-      
+        computed: {
+            routes() {
+                return this.$store.state.routes;
+            },
+            user() {
+                return this.$store.state.currentHr;
+            }
+        },
+        methods: {
+            goChat() {
+                this.$router.push("/chat");
+            },
+            commandHandler(cmd) {
+                if (cmd == 'logout') {
+                    this.$confirm('此操作将注销登录, 是否继续?', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }).then(() => {
+                        this.getRequest("/logout");
+                        window.sessionStorage.removeItem("user")
+                        this.$store.commit('initRoutes', []);
+                        this.$router.replace("/");
+                    }).catch(() => {
+                        this.$message({
+                            type: 'info',
+                            message: '已取消操作'
+                        });
+                    });
+                }else if (cmd == 'userinfo') {
+                    this.$router.push('/hrinfo');
+                }
+            }
+        }
     }
 </script>
 
 <style>
+    .container{
+        width:100%;
+        height:100px;
+    }
     .homeRouterView {
         margin-top: 10px;
     }
